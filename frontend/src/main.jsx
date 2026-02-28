17702ac0379cd517454d715a4f7ce7d95f3727fd
Tin Pham <phtintin2004@gmail.com>
2026-02-28 04:25:43 -0500

    fixing ci pipeline

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
-import "./styles/app.css";
+import "./styles/App.css";
 import AppProviders from "./app/App.jsx";
 import AppRouter from "./app/router.jsx";
 import Header from "./shared/components/header/header.jsx";
